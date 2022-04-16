8f9f79f9ecb4aa6c64513f07aef15c5b65ad5350
Xusheng <xusheng@vector35.com>
2022-04-16 16:47:27 +0800

    Update build script and CMakeLists.txt

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ import platform
 import sys
 import os
 import subprocess
+import zipfile
 
 
 def run_cmd(cmd: str) -> bool:
@@ -14,35 +15,61 @@ def run_cmd(cmd: str) -> bool:
     return True
 
 
+def create_archive():
+    print("\nCreating archive...")
+    with zipfile.ZipFile('binaries.zip', 'w', zipfile.ZIP_DEFLATED) as z:
+        for root, dirs, files in os.walk('binaries'):
+            for file in files:
+                print(f"Adding {root}/{file}...")
+                z.write(os.path.join(root, file), os.path.join(root, file))
+
+
 def mac_sign():
     pass
 
 
 def mac_build():
-    build_cmd = f"cmake -DARCH=arm64 . && make"
+    if not os.path.exists('build/arm64'):
+        os.makedirs('build/arm64')
+
+    build_cmd = f"cmake -B build/arm64 -DARCH=arm64 . && cd build/arm64 && make"
     if not run_cmd(build_cmd):
         return False
 
-    build_cmd = f"cmake -DARCH=x86_64 . && make"
+    if not os.path.exists('build/x86_64'):
+        os.makedirs('build/x86_64')
+
+    build_cmd = f"cmake -B build/x86_64 -DARCH=x86_64 . && cd build/x86_64 && make"
     if not run_cmd(build_cmd):
         return False
 
+    create_archive()
     return True
 
 
 def linux_build():
-    build_cmd = f"cmake -DARCH=x86 . && make"
+    if not os.path.exists('build/x86'):
+        os.makedirs('build/x86')
+
+    build_cmd = f"cmake -B build/x86 -DARCH=x86 . && cd build/x86 && make"
     if not run_cmd(build_cmd):
         return False
 
-    build_cmd = f"cmake -DARCH=x86_64 . && make"
+    if not os.path.exists('build/x86_64'):
+        os.makedirs('build/x86_64')
+
+    build_cmd = f"cmake -B build/x86_64 -DARCH=x86_64 . && cd build/x86_64 && make"
     if not run_cmd(build_cmd):
         return False
 
+    create_archive()
     return True
 
 
 def windows_build():
+    if not os.path.exists('build/x86_64'):
+        os.makedirs('build/x86_64')
+
     vcvars = subprocess.check_output(fR"""call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat" && set""", shell=True)
     for line in vcvars.split(b'\r\n'):
         line = line.strip()
@@ -53,10 +80,9 @@ def windows_build():
         value = b'='.join(parts[1:]).decode()
         os.environ[key] = value
 
-    build_cmd = f"cmake -G \"NMake Makefiles\" -DCMAKE_BUILD_TYPE=Release -DARCH=x86_64 . && nmake"
+    build_cmd = f"cmake -B build/x86_64 -G \"NMake Makefiles\" -DCMAKE_BUILD_TYPE=Release -DARCH=x86_64 . && cd build/x86_64 && nmake"
     if not run_cmd(build_cmd):
         return False
-
 
     vcvars = subprocess.check_output(fR"""call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars32.bat" && set""", shell=True)
     for line in vcvars.split(b'\r\n'):
@@ -68,10 +94,11 @@ def windows_build():
         value = b'='.join(parts[1:]).decode()
         os.environ[key] = value
 
-    build_cmd = f"cmake -G \"NMake Makefiles\" -DCMAKE_BUILD_TYPE=Release -DARCH=x86 . && nmake"
+    build_cmd = f"cmake -B build/x86 -G \"NMake Makefiles\" -DCMAKE_BUILD_TYPE=Release -DARCH=x86 . && cd build/x86 && nmake"
     if not run_cmd(build_cmd):
         return False
 
+    create_archive()
     return True
 
 
